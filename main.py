3d2bdb43ae472468ae05bff4f10923dfda26dec7
vope <kkeea1020@gmail.com>
2024-03-16 22:41:05 +0800

    filter attractions data

--- main.py
+++ main.py
@@ -6,22 +6,34 @@ attractions_data = []
 headers = {
     'accept' : 'application/json'
 }
+Attractions_filter = ["address", "category", "elong", "nlat", "email", "images", "introduction", "name", "official_site", "open_time", "remind", "service", "tel", "ticket", "url"]
+def filter(data):
+    new_data = []
+    for d in data:
+        new_dict = dict()
+        for k, v in d.items():
+            if k in Attractions_filter:
+                new_dict.update({k : v})
+        new_data.append(new_dict) 
 
+    return new_data 
+            
 # Get Event Data
-def Get_Event_Data():
+def Get_Attractions_Data():
     api_base_url = "https://www.travel.taipei/open-api/zh-tw/Attractions/All"
     try:
         response = requests.get(api_base_url, headers=headers)
         if response.status_code == 200:
             data = response.json()
-            return data
+            new_data = filter(data["data"])
+            return new_data
     except Exception as e:
         return f'error:${e}'
 
 # 回傳Data
 @app.route("/attractions", methods=["GET"])
 def send_attractions_data():
-    attractions_data = Get_Event_Data()
+    attractions_data = Get_Attractions_Data()
     return attractions_data
 
 if __name__ == '__main__':
