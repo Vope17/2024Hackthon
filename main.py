69c6b08a4955bf5bbd05c4f346da39f2bb0d083f
vope <kkeea1020@gmail.com>
2024-03-16 17:28:34 +0800

    獲得景點資訊

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from flask import Flask, request
-
+from flask import Flask, request, jsonify
+import requests
 app = Flask(__name__)
 
-@app.route('/example', methods=['POST', 'GET'])
-def hello():
-    req_data = request.get_json()
+attractions_data = []
+headers = {
+    'accept' : 'application/json'
+}
 
-    hi = req_data['hi']
-    xd = req_data['xd']
-    return '''the msg is {}, the msg2 is {}'''.format(hi, xd) 
+# Get Event Data
+def Get_Event_Data():
+    api_base_url = "https://www.travel.taipei/open-api/zh-tw/Attractions/All"
+    try:
+        response = requests.get(api_base_url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            return data
+    except Exception as e:
+        return f'error:${e}'
+
+# 回傳Data
+@app.route("/attractions", methods=["GET"])
+def send_attractions_data():
+    attractions_data = Get_Event_Data()
+    return attractions_data
 
 if __name__ == '__main__':
     app.run(debug=True)
+    print(attractions_data)
